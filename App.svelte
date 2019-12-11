4ca082db6d197ef5b9fe967bb608bb7216300b7e
Alexislds37 <39832572+Alexislds37@users.noreply.github.com>
2019-12-11 15:28:42 +0100

    Add files via upload

---
+++ App.svelte
@@ -0,0 +1,15 @@
+<script>
+    let books = [];
+    import TinyDB from './TinyDB.svelte';
+</script>
+
+<main>
+    <TinyDB bind:documents={books} initsrc="./books.json" collection="books"/>
+    <h1>Tiny Amazon</h1>
+    <ul>
+        {#each books as b}
+        <li>{b.title}</li>
+        {/each}
+    </ul>
+
+</main>
